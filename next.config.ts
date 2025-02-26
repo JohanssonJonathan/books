98dec24d964ca4327d40a47cb5d33b715da61d80
Jonte <jonte@Mac.lan>
2025-02-26 15:02:39 +0100

    remove cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: () =>
+    Promise.resolve([
+      {
+        source: '/*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ]),
 };
-
 export default nextConfig;
