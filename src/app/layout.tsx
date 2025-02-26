8b3cdfde227d1684b08b37bcb0e5a65e275cb05c
Jonte <jonte@Mac.lan>
2025-02-26 15:08:30 +0100

    remove cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './_frontend/globals.css';
 
+export const fetchCache = 'force-no-store';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
