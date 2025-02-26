c74e31bbfa075963395ce7d2b1c5d7f7e5320771
Jonte <jonte@Mac.lan>
2025-02-26 22:13:26 +0100

    move out of variable

--- src/app/_frontend/util/ReactQueryProvider.tsx
+++ src/app/_frontend/util/ReactQueryProvider.tsx
@@ -3,15 +3,17 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 
-export const defaultQueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 0,
-    },
-  },
-});
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => defaultQueryClient);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            gcTime: 0,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
